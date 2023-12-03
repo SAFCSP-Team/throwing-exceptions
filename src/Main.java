a825f293e214e7311b712dda287c811acb94d312
nourabyte <148945652+nourabyte@users.noreply.github.com>
2023-12-03 13:26:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
+    public static void main(String args[]){   
+   int age = 13;
+  }
 
-    public static void main(String[] args) {
-    }
+  public static void ageValidator(int age)   
+    {
 
-}
+        }
+}  
